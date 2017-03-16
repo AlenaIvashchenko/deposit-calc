ec7ce06b467ed3b814600470b6f04720d8f233c1
Ivashchenko Alena <alenshik1997god@mail.ru>
2017-03-16 10:02:27 +0700

    creat fail

---
+++ calc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    void getInput(int*, float*);
+    void perform(int*, float*);
+    
+    int main(void) {
+        int duration;
+        float investment;
+        
+
+        printf("Enter investment duration: ");
+        scanf("%d", &duration);
+        printf("Enter investent amount: ");
+        scanf("%f", &investment );
+} 
