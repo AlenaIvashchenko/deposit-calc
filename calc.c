d1d7931008c018447f75aa20ebd0d35a32defe98
Ivashchenko Alena <alenshik1997god@mail.ru>
2017-03-16 10:29:54 +0700

    add void perform

--- calc.c
+++ calc.c
@@ -26,4 +26,26 @@
         if(*dur < 0 || *dur > 365 || *amount < 0.0) {
         getInput(dur, amount);
        }
+    } 
+    
+    void perform(int* dur, float* amount) {
+          if (*dur >= 0 && *dur <= 30) {
+            *amount *= 0.9;
+      }
+        
+       if (*amount < 100000) {
+          if (*dur >= 31 && *dur <= 120) {
+              *amount *=1.02;
+      }
+
+
+       if(*dur >= 121 && *dur <= 240) {
+          *amount *=1.06;
+      }
+
+       if(*dur >= 241 && *dur <= 365) {
+          *amount *=1.12;
+      }
     }
+
+
